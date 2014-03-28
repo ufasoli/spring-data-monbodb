a04888811a841e89344d8a4e0d4c2d1757d4878d
Ulises Fasoli <ufasoli@gmail.com>
2014-03-28 10:21:14 +0100

    Typo in the repository marked wrongly as @RestController

--- src/main/java/com/ufasoli/repositories/jpa/OrderRepository.java
+++ src/main/java/com/ufasoli/repositories/jpa/OrderRepository.java
@@ -1,12 +1,7 @@
 package com.ufasoli.repositories.jpa;
 
-import com.ufasoli.model.jpa.Customer;
 import com.ufasoli.model.jpa.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 24.03.2014
  * Time: 16:52
  */
-@RestController
-@RequestMapping(value="/orders",  produces = MediaType.APPLICATION_JSON_VALUE )
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
