4eb3f431ba55e70188ca8db2dd4fddfbc8f86591
Ulises Fasoli <ufasoli@gmail.com>
2014-03-28 09:51:25 +0100

    Added support for spring-data-rest

--- src/main/java/com/ufasoli/Application.java
+++ src/main/java/com/ufasoli/Application.java
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.MongoTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -33,6 +31,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableJpaRepositories (basePackages = "com.ufasoli.repositories.jpa")
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.ufasoli.repositories.*"))
 @EnableAutoConfiguration(exclude = {MongoRepositoriesAutoConfiguration.class})
+@Import(RepositoryRestMvcConfiguration.class)
 public class Application {
 
     public static void main(String[] args){
--- src/main/java/com/ufasoli/repositories/mongo/BookRepository.java
+++ src/main/java/com/ufasoli/repositories/mongo/BookRepository.java
@@ -3,6 +3,8 @@ package com.ufasoli.repositories.mongo;
 import com.ufasoli.model.mongo.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * User: Ulises Fasoli
@@ -10,8 +12,9 @@ import org.springframework.data.querydsl.QueryDslPredicateExecutor;
  * Time: 14:58
  * Project : spring-data-monbodb
  */
+@RepositoryRestResource(collectionResourceRel = "books", path = "rest-books")
 public interface BookRepository extends MongoRepository<Book, String>,
         BookRepositoryCustom, QueryDslPredicateExecutor<Book> {
 
-    public Iterable<Book> findByGenre(String genre);
+    public Iterable<Book> findByGenre(@Param("genre") String genre);
 }
